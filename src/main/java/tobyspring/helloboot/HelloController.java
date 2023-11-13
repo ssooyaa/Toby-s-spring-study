603c35492ce46a68f35812cc43b886b764a68ca3
ssooyaa <tvfxq0214@gmail.com>
2023-11-13 18:22:32 +0900

    컨트롤러 어노테이션 매핑 정보 사용

--- src/main/java/tobyspring/helloboot/HelloController.java
+++ src/main/java/tobyspring/helloboot/HelloController.java
@@ -1,17 +1,24 @@
 package tobyspring.helloboot;
 
 import java.util.Objects;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@RequestMapping("/hello")
 public class HelloController {
+
     private final HelloService helloService;
 
     public HelloController(HelloService helloService) {
         this.helloService = helloService;
     }
 
+    @GetMapping
+    @ResponseBody
     public String hello(String name) {
         return helloService.sayHello(Objects.requireNonNull(name));
     }
+//    requestcontroller의 경우 ResponseBody가 기본 설정 되어있음
 }
