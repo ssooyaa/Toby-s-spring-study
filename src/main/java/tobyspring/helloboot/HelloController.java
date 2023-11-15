3b1768f610e4d829922e9b30cefd443beee9e463
ssooyaa <tvfxq0214@gmail.com>
2023-11-15 23:22:51 +0900

    단위테스트 코드 작성

--- src/main/java/tobyspring/helloboot/HelloController.java
+++ src/main/java/tobyspring/helloboot/HelloController.java
@@ -11,16 +11,17 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
 
     private final HelloService helloService;
-    private ApplicationContext applicationContext;
 
-    public HelloController(HelloService helloService, ApplicationContext applicationContext) {
+    public HelloController(HelloService helloService) {
         this.helloService = helloService;
-        this.applicationContext = applicationContext;
     }
 
     @GetMapping("/hello")
     public String hello(String name) {
-        return helloService.sayHello(Objects.requireNonNull(name));
+        if (name == null || name.trim().length() == 0) {
+            throw new IllegalArgumentException();
+        }
+        return helloService.sayHello(name);
     }
 //    requestcontroller의 경우 ResponseBody가 기본 설정 되어있음
 }
