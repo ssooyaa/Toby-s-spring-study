5abd1856185320db60c6ebfb202034ac7afd9f5e
ssooyaa <tvfxq0214@gmail.com>
2023-11-21 15:47:05 +0900

    컴포스트애노테이션

--- src/test/java/tobyspring/helloboot/HelloServiceTest.java
+++ src/test/java/tobyspring/helloboot/HelloServiceTest.java
@@ -1,11 +1,29 @@
 package tobyspring.helloboot;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+@UniTest
+@interface FastUnitTest {
+ //컴포스트 애노테이션 = 메타 애노테이션을 하나이상을 적용해서 만드는 경우
+}
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.ANNOTATION_TYPE, ElementType.METHOD}) //ElementType.ANNOTATION_TYPE 포함되어야 UniTest메타 어노테이션으로 사용가능
+@Test
+@interface UniTest {
+
+}
+
 public class HelloServiceTest {
 
-    @Test
+    @UniTest
     void simpleHelloService() {
         SimpleHelloService helloService = new SimpleHelloService();
 
