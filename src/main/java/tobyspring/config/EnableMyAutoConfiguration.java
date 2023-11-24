6cf7c2597bc0a0035ccc1243632e229f370d0819
ssooyaa <tvfxq0214@gmail.com>
2023-11-24 21:37:57 +0900

    자동구성 테스트

--- src/main/java/tobyspring/config/EnableMyAutoConfiguration.java
+++ src/main/java/tobyspring/config/EnableMyAutoConfiguration.java
@@ -1,21 +1,16 @@
-package tobyspring.helloboot;
+package tobyspring.config;
 
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
-import tobyspring.config.EnableMyAutoConfiguration;
 import tobyspring.config.aoutoconfig.DispatcherServletConfig;
 import tobyspring.config.aoutoconfig.TomcatWebServerConfig;
 
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
-@Configuration
-@ComponentScan
-@EnableMyAutoConfiguration
-public @interface   MySpringBootApplication {
+@Import(MyAutoConfigImportSelector.class )
+public @interface EnableMyAutoConfiguration {
 
 }
