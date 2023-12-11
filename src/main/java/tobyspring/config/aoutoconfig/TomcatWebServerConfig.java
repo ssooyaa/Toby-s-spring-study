b0f8ee32922d28defacd2de62569ec6e940c057f
ssooyaa <tvfxq0214@gmail.com>
2023-12-11 20:54:53 +0900

    properties 적용

--- src/main/java/tobyspring/config/aoutoconfig/TomcatWebServerConfig.java
+++ src/main/java/tobyspring/config/aoutoconfig/TomcatWebServerConfig.java
@@ -1,21 +1,32 @@
 package tobyspring.config.aoutoconfig;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Conditional;
+import org.springframework.core.env.Environment;
 import tobyspring.config.ConditionalMyOnClass;
 import tobyspring.config.MyAutoConfiguration;
 
 @MyAutoConfiguration
-@ConditionalMyOnClass("rg.apache.catalina.startup.Tomcat")
+@ConditionalMyOnClass("org.apache.catalina.startup.Tomcat")
 public class TomcatWebServerConfig {
+
+//    @Value("${contextPath}")
+//    String contextPath;
+//
+//    @Value("${port:8080}")
+//    int port;
+    //없을 시 디폴트 값 사용
 
     @Bean("tomcatWebServerFactory")
     @ConditionalOnMissingBean
-    public ServletWebServerFactory servletWebServerFactory() {
-        return new TomcatServletWebServerFactory();
+    public ServletWebServerFactory servletWebServerFactory(ServerProperties properties) {
+        TomcatServletWebServerFactory factory = new TomcatServletWebServerFactory();
+        factory.setContextPath(properties.getContextPath());
+        factory.setPort(properties.getPort());
+        return factory;
     }
-
 }
--- src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -1,12 +1,26 @@
 package tobyspring.helloboot;
 
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
+
 @MySpringBootApplication
 public class HellobootApplication {
+
+    @Bean
+    ApplicationRunner applicationRunner(Environment env) {
+        return args -> {
+            String name = env.getProperty("my.name");
+            System.out.println("my.name: " + name);
+        };
+    }
 
     public static void main(String[] args) {
 //        GenericWebApplicationContext는 java로 만든 Configuration 정보를 읽을 수 없다.
 
-        MySpringApplication.run(HellobootApplication.class, args);
+        SpringApplication.run(HellobootApplication.class, args);
 
     }
 
