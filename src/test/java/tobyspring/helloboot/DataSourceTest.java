a5a8c60bd8d6f66e439c3ae9da17a1a4cb3415d5
ssooyaa <tvfxq0214@gmail.com>
2023-12-15 00:07:15 +0900

    JdbcTemplate, 트랜잭션 매니저 구성

---
+++ src/test/java/tobyspring/helloboot/DataSourceTest.java
@@ -0,0 +1,25 @@
+package tobyspring.helloboot;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import javax.sql.DataSource;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.transaction.annotation.Transactional;
+
+@HellobootTest
+public class DataSourceTest {
+    @Autowired
+    DataSource dataSource;
+
+    @Test
+    void connect() throws SQLException{
+        Connection connection = dataSource.getConnection();
+        connection.close();
+    }
+}
